e223749559efa7d5186363dd9cf75a90bff7cb63
W1l50n2208 <davide.paduanelli@gmail.com>
2020-02-14 17:22:32 +0100

    commentato il funzionamento della funzione plot_results in tetris_utils

--- ML_Tetris/tetris_utils.py
+++ ML_Tetris/tetris_utils.py
@@ -1,27 +1,30 @@
 import matplotlib.pyplot as plt
 
-def plot_results(scoreArray, game_index_array, w0, w1, w2, w3):
-    plt.figure(1)
-    plt.subplot(211)
-    plt.plot(game_index_array, scoreArray, 'k-')
-    plt.xlabel('Game Number')
-    plt.ylabel('Game Score')
-    plt.title('Learning Curve')
-    plt.xlim(1, max(game_index_array))
-    plt.ylim(0, max(scoreArray) * 1.1)
 
-    # Plot the weights over time
-    plt.subplot(212)
-    plt.xlabel('Game Number')
-    plt.ylabel('Weights')
-    plt.title('Learning Curve')
-    ax = plt.gca()
-    ax.set_yscale('log')
-    plt.plot(game_index_array, w0, label="Aggregate Height")
-    plt.plot(game_index_array, w1, label="Unevenness")
-    plt.plot(game_index_array, w2, label="Maximum Height")
-    plt.plot(game_index_array, w3, label="Number of Holes")
-    plt.legend(loc='lower left')
-    plt.xlim(0, max(game_index_array))
-    plt.ylim(0.0001, 100)
-    plt.show()
+# questa funzione può essere utilizzata per tutti i tipi di AI, eventualmente aggiungo un parametro che in base al tipo
+# di IA cambia la tipologia di grafico da disegnare
+def plot_results(scoreArray, game_index_array, w0, w1, w2, w3):
+    plt.figure(1)  # crea la finestra in cui stampare i grafici
+    plt.subplot(211)  # descrive la posizione dei subplot, con 211 si riferisce al grafico che sta in alto
+    plt.plot(game_index_array, scoreArray, 'k-')  # disegna il grafico utilizzando i parametri che gli vengono passati
+    plt.xlabel('Game Number')  # assegna un nome all'asse delle x
+    plt.ylabel('Game Score')  # assegna un nome all'asse delle y
+    plt.title('Learning Curve')  # titolo del grafico che sta in alto
+    plt.xlim(1, max(game_index_array))  # definisce il valore minimo e il valore massimo che l'asse delle x dovrà avere
+    plt.ylim(0, max(scoreArray) * 1.1)  # definisce il valore minimo e il valore massimo che l'asse delle y dovrà avere
+
+    # disegna il grafico dei pesi man mano
+    plt.subplot(212)  # descrive la posizione dei subplot, con 212 si riferisce al grafico che sta in basso
+    plt.xlabel('Game Number')  # nome dell'asse delle x
+    plt.ylabel('Weights')  # nome dell'asse delle y
+    plt.title('Learning Curve')  # titolo delle secondo grafico
+    ax = plt.gca()  # restituisce gli assi del grafico creato precedentemente
+    ax.set_yscale('log')  # serve per settare lo scaling delle y, in questo caso è logaritmico
+    plt.plot(game_index_array, w0, label="Aggregate Height")  # funzione che mostra l'andamento dei pesi aggregati
+    plt.plot(game_index_array, w1, label="Unevenness")  # funzione che mostra l'andamento dell'unevenness
+    plt.plot(game_index_array, w2, label="Maximum Height")  # funzione che mostra l'andamento dell'altezza massima
+    plt.plot(game_index_array, w3, label="Number of Holes")  # funzione che mostra l'andamento del numero di buchi
+    plt.legend(loc='lower left')  # pone la legenda in basso a sinistra
+    plt.xlim(0, max(game_index_array))  # definisce il valore minimo e il valore massimo che l'asse delle x dovrà avere
+    plt.ylim(0.0001, 100)  # definisce il valore minimo e il valore massimo che l'asse delle y dovrà avere
+    plt.show()  # stampa a schermo i grafici
