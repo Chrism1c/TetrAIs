4fab4765d4122ebb401cfb03a1e37fbdf59ae194
Chrism1c <christian-miccolis@libero.it>
2020-03-06 16:56:53 +0100

    - fix Exit shortcut - Implementato DFS LV1 e LV2

--- com/Agents/LocalSearch.py
+++ com/Agents/LocalSearch.py
@@ -3,68 +3,24 @@ from abc import ABC
 import copy
 from com.Core.Utils import simulate_board, get_parameters
 from com.Core.Model import PIECES
-import sys
+import sys,time
 
 class LocalSearch(BaseGame, ABC):
-    def __init__(self, r_p, lv):
+    def __init__(self, r_p):
         super().__init__(r_p)
-        self.lv = lv
 
     def get_move(self):
-        if self.lv == 'LV1':
-            return self.find_best_moveLS_LV1only(self.board, self.falling_piece)
-        else:
-            return self.find_best_moveLS_full(self.board, self.falling_piece, self.next_piece)
+        return self.find_best_moveLS(self.board, self.falling_piece, self.next_piece)
 
 
-    def find_best_moveLS_full(self, board, piece, NextPiece):
+    def find_best_moveLS(self, board, piece,NextPiece):
         ### Cerca la mossa migliore da effettuare sulla board, passando il vettore dei pesi
-        #start = time.perf_counter()  # salvo il tempo di partenza
 
-        best_rot = 0
-        best_sideways = 0
-        best_score = - 99
-
-        NextScore = (0, 0, -99)  # rot,sideways, score
-        bestLines = -1
-        nextLines = -1
-
-        # rot =  1-'O':    2-'I': 2-'Z':    4-'J': 4-'L': 4-'T'
-
-        for rot in range(0, len(PIECES[piece['shape']])):  # per le rotazioni possibili su lpezzo corrente
-            for sideways in range(-5, 6):  # per i drop possibili sulla board
-                move = [rot, sideways]  # salvo la coppia corrente
-                test_board = copy.deepcopy(board)  # duplico la board corrente
-                test_piece = copy.deepcopy(piece)  # duplico il pezzo corrente
-                test_board = simulate_board(test_board, test_piece, move)  # simulo il pezzo e la mossa sulla board test
-                # Check NEXT
-                if test_board is not None:  # se la simulazione è andata a buon fine
-                    ## Chose the best after next                                # effettuo il calcolo con il pezzo successivo
-                    for rot2 in range(0, len(PIECES[NextPiece['shape']])):
-                        for sideways2 in range(-5, 6):
-                            move2 = [rot2, sideways2]
-                            test_board2 = copy.deepcopy(test_board)
-                            test_piece2 = copy.deepcopy(NextPiece)
-                            test_board2 = simulate_board(test_board2, test_piece2, move2)
-                            if test_board2 is not None:
-                                test_score2, nextLines = self.get_expected_score(test_board2)
-                                if NextScore[2] < test_score2:
-                                    NextScore = [rot2, sideways2, test_score2]  # aggiorno il best local score (LV2)
-                    if best_score < NextScore[2]:  # confronto
-                        best_score = NextScore[2]  # aggiorno il best local score (LV1+LV2)
-                        best_sideways = sideways  # aggiorno il best sideway (LV1)
-                        best_rot = rot  # aggiorno il best rot (LV1)
-
-        #finish = time.perf_counter()
-        #print(f'Finished in {round(finish - start, 2)} second(s) with full')
-
-        return [best_rot, best_sideways]
-
-    def find_best_moveLS_LV1only(self, board, piece):
-        ### Cerca la mossa migliore da effettuare sulla board, passando il vettore dei pesi
-        #start = time.perf_counter()
-
-        strategy = None
+        #print("-----------------------------------------------------------------------------")
+        #print("******************************* ",piece['shape'],"   *********************************")
+        ### Cerco il max locale LV1
+        strategy = (0,0,-999)
+        best_board = None
         for rot in range(0, len(PIECES[piece['shape']])):
             for sideways in range(-5, 6):
                 move = [rot, sideways]
@@ -72,14 +28,37 @@ class LocalSearch(BaseGame, ABC):
                 test_piece = copy.deepcopy(piece)
                 test_board = simulate_board(test_board, test_piece, move)
                 if test_board is not None:
-                    test_score = self.get_expected_score(test_board)
+                    test_score, fullLines = self.get_expected_score(test_board)
+                    #print(move," -- ",strategy[2],"<", test_score)
                     if not strategy or strategy[2] < test_score:
+                        #print("update = new max LV1")
                         strategy = (rot, sideways, test_score)
+                        best_board = test_board
 
+        #print("LV1 Winner is: ",strategy)
+        #time.sleep(2)
+        #print("******************************* ", NextPiece['shape'], "   *********************************")
+        ### Cerco il Max Locale LV2
+        strategy2 = (0,0,-999)
+        for rot in range(0, len(PIECES[NextPiece['shape']])):
+            for sideways in range(-5, 6):
+                move2 = [rot, sideways]
+                test_board2 = copy.deepcopy(best_board)
+                test_piece2 = copy.deepcopy(NextPiece)
+                test_board2 = simulate_board(test_board2, test_piece2, move2)
+                if test_board2 is not None:
+                    test_score2, fullLines2 = self.get_expected_score(test_board2)
+                    #print(move2," -- ", strategy2[2], "<", test_score2)
+                    if not strategy2 or strategy2[2] < test_score2:
+                        #print("update = new max LV2")
+                        strategy2 = (rot, sideways, test_score2)
+
+        #print("LV2 Winner is: ", strategy2)
         #finish = time.perf_counter()
         #print(f'Finished in {round(finish - start, 2)} second(s) with LV1Only')
 
-        return [strategy[0], strategy[1]]
+        return [strategy2[0], strategy2[1]]
+
 
     def get_expected_score(self, test_board):
         ### Calcola lo score sulla board di test passando il vettore dei pesi di ogni metrica
@@ -92,10 +71,9 @@ class LocalSearch(BaseGame, ABC):
 
 if __name__ == "__main__":
     r_p = sys.argv[1]
-    lv = sys.argv[2]
-    numOfRun = int(sys.argv[3])
+    numOfRun = int(sys.argv[2])
     for x in range(numOfRun):
-        ls = LocalSearch(r_p, lv)
+        ls = LocalSearch(r_p)
         newScore, weights = ls.run()
         print("Game achieved a score of: ", newScore)
         print("weights ", weights)
