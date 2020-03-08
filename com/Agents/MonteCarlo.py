56194c5b3811a31a8189793583d1d69cac7fde9f
W1l50n2208 <davide.paduanelli@gmail.com>
2020-03-08 16:49:09 +0100

    configurazioni iniziali del MonteCarlo

---
+++ com/Agents/MonteCarlo.py
@@ -0,0 +1,13 @@
+import time
+import math
+import random
+import copy
+import numpy as np
+import operator
+
+from com.Core.BaseGame import *
+from com.Core.Model import *
+from com.Core.Plot import *
+
+PSEUDO_INFINITY = 1000000000000.0
+
