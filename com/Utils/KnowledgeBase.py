72935c2f6517361beec10360835741a3b0a70753
Mattia Patruno <45966780+m3ttiw@users.noreply.github.com>
2020-03-10 18:53:05 +0100

    Creata la classe Fact

--- com/Utils/KnowledgeBase.py
+++ com/Utils/KnowledgeBase.py
@@ -1,2 +1,1 @@
 from com.Utils.KnowledgeBaseScheme import *
-
--- com/Utils/KnowledgeBaseScheme.py
+++ com/Utils/KnowledgeBaseScheme.py
@@ -1,3 +1,20 @@
 from collections import namedtuple
 
-fact = namedtuple('relation', 'x', 'y')
+fact = namedtuple('fact', 'relation x y')
+
+
+class Fact:
+    def __init__(self, relation: str, x: str, y: str = None):
+        self.relation = relation
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        if self.y is not None:
+            return self.relation + '(' + self.x + ', ' + self.y + ')'
+        else:
+            return self.relation + '(' + self.x + ')'
+
+
+class Rule:
+    def __init__(self):
