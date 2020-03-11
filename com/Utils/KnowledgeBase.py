9582bdf35af0d22239e73cc2404b4ae0a110b7ef
Mattia Patruno <45966780+m3ttiw@users.noreply.github.com>
2020-03-11 18:56:13 +0100

    Codificate le shadows

--- com/Utils/KnowledgeBase.py
+++ com/Utils/KnowledgeBase.py
@@ -1,4 +1,3 @@
-from com.Utils.KnowledgeBaseScheme import *
 from collections import namedtuple
 
 #codifica le regole semplici
@@ -21,15 +20,71 @@ differences = {
 
 #codifica le regole composte
 
+#crea dizionario con le regole
+
 #grandezza 4
-flat_4 = ('flat', 'flat', 'flat', 'flat')
+f_4 = ('flat', 'flat', 'flat', 'flat')
+
 
 #grandezza 3
-flat_3 = ('flat', 'flat', 'flat')
-
+f_3 = ('flat', 'flat', 'flat')
+f_d_u = ('flat', 'down', 'up')
+f_f_u = ('flat', 'flat', 'up')
+f_d_f = ('flat', 'down', 'flat')
+f_u_f = ('flat', 'up', 'flat')
+f_f_d = ('flat', 'flat', 'down')
 
 
 #grandezza 2
+f_2 = ('flat', 'flat')
+f_d = ('flat', 'down')
+f_u = ('flat', 'up')
+f_jd = ('flat', 'jump_down')
+f_ju = ('flat', 'jump_up')
 
 
+#grandezza 1
+f_1 = ('flat')
 
+shadows_S = {
+    f_f_u: 1,
+    f_d: 2
+}
+
+shadows_Z = {
+    f_d_f: 1,
+    f_u: 2
+}
+
+
+shadows_J = {
+    f_f_d: 1,
+    f_ju: 2,
+    f_3: 3,
+    f_2: 4
+}
+
+
+shadows_L = {
+    f_u_f: 1,
+    f_jd: 2,
+    f_3: 3,
+    f_2: 4
+}
+
+
+shadows_I = {
+    f_1: 1,
+    f_4: 2,
+}
+
+shadows_O = {
+    f_2: 1
+}
+
+shadows_T = {
+    f_d_u: 1,
+    f_d: 2,
+    f_u: 3,
+    f_3: 4,
+}
--- com/Utils/KnowledgeBaseScheme.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-
-
-
-
