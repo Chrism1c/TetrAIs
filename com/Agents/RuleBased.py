84828734b0ff72ba58b30ba87e9cdbe8e5ca2de6
Mattia Patruno <45966780+m3ttiw@users.noreply.github.com>
2020-03-13 19:16:27 +0100

    Create le shadows, utilizzando l'insieme delle parti della cresta per confrontare le shadows

--- com/Agents/RuleBased.py
+++ com/Agents/RuleBased.py
@@ -5,281 +5,33 @@ from com.Core.Model import BOARDHEIGHT, BOARDWIDTH, is_on_board
 class RuleBased(BaseGame):
     def __init__(self, r_p):
         super().__init__(r_p)
+        self.crest = [0]*BOARDWIDTH                 #cresta relativa
 
     def get_move(self):
-        crest = self.get_crest()
-        rule = self.get_rule_by_crest(self.falling_piece)
-        return self.align(rule(crest))
+        #self.update_crest(self.get_heights())
+        self.crest = self.get_heights()
+        rotation, start = self.get_move_by_rule(self.falling_piece, self.get_Pcrest())()
+        return [rotation, start-5]
 
-    #non utilizzata
-    def bin_matrix_board(self):
-        bin_matrix_board_ = list()
-        for y in range(BOARDHEIGHT):
-            line = [0] * BOARDWIDTH
-            for x in range(BOARDWIDTH):
-                if is_on_board(x, y):
-                    line[x] = 1
-            bin_matrix_board_.append(line)
-        return bin_matrix_board_
+    def get_move_by_rule(self, piece, Pa):
 
-    def get_crest(self):
-        crest = [0] * BOARDWIDTH
-        for x in range(BOARDWIDTH):  # per ogni colonna
-            maxH = 0
-            for y in range(BOARDHEIGHT):  # per ogni riga
-                if is_on_board(x, y):
-                    if maxH < y:
-                        maxH = y
-            crest[x] = maxH
-        return crest
-
-    #ricontrollare i pezzi
-    def get_rule_by_crest(self, piece):
-
-        def rule_S(crest):
-            # 2 flat 1 up altimenti 1 flat 1 down
-            y_flat = crest[0]
-            count_flat = 1
-            pos1 = -1
-            pos2 = -1
-            for x in range(1, len(crest)):
-                if count_flat == 2 and crest[x] == y_flat + 1:
-                    pos1 = x - 1
-                elif crest[x] == y_flat - 1:
-                    pos2 = x
-                elif y_flat == crest[x]:
-                    count_flat += 1
-                else:
-                    y_flat = crest[x]
-                    count_flat = 1
-                    flat = x
-            if pos1 != -1:
-                # metti il pezzo in pos1
-                pass
-            elif pos2 != -1:
-                # metti il pezzo in pos2
-                pass
-            elif count_flat > 2:
-                # metti il pezzo nella posizione 1 a partire da flat
-                pass
-            else:
-                # metti il pezzo in min(crest[x]) o random
-                pass
-
-        def rule_Z(crest):
-            # 1 flat 1 up altimenti 1 flat 1 down 1 flat
-            y_flat = crest[0]
-            count_flat_1 = 1
-            count_flat_2 = 0
-            pos1 = -1
-            pos2 = -1
-            for x in range(1, len(crest)):
-                if count_flat_1 == 1 and crest[x] == y_flat + 1:
-                    pos1 = x - 1
-                    y_flat = crest[x]
-                    count_flat_1 = 1
-                elif crest[x] == y_flat - 1:
-                    count_flat_2 = 1
-                    y_flat = crest[x]
-                    count_flat_1 = 1
-                elif y_flat == crest[x]:
-                    count_flat_1 += 1
-                elif y_flat == crest[x] and count_flat_2 == 1:
-                    pos2_x = x - 2
-                else:
-                    y_flat = crest[x]
-                    count_flat_1 = 1
-                    flat = x
-            if pos1 != -1:
-                # metti il pezzo in pos1
-                pass
-            elif pos2 != -1:
-                # metti il pezzo in pos2
-                pass
-            elif count_flat_1 > 2:
-                # metti il pezzo nella posizione 1 a partire da flat
-                pass
-            else:
-                # metti il pezzo in min(crest[x]) o random
-                pass
-
-        def rule_J(crest):
-            y_flat = crest[0]
-            count_flat = 1
-            pos1 = -1  # 2 flat 1 down
-            pos2 = -1  # 1 flat 1 jump
-            pos3 = -1  # 3 flat
-            pos4 = -1  # 2 flat
-            for x in range(1, len(crest)):
-                if crest[x] == y_flat:
-                    count_flat += 1
-                else:
-                    if crest[x] == y_flat - 1 and count_flat == 2:
-                        pos1 = x - 2
-                    elif crest[x] == y_flat + 2:
-                        pos2 = x - 1
-                    y_flat = crest[x]
-                    count_flat = 1
-                if count_flat == 3:
-                    pos3 = x - 2
-                if count_flat == 2:
-                    pos4 == x - 1
-
-            if pos1 != -1:
-                pass
-            elif pos2 != -1:
-                pass
-            elif pos3 != -1:
-                pass
-            elif pos4 != -1:
-                pass
-            else:
-                # random
-                pass
-
-        def rule_L(crest):
-            y_flat = crest[0]
-            count_flat = 1
-            up = 0
-            pos1 = -1  # 1 flat 2 up
-            pos2 = -1  # 1 flat 1 jump down
-            pos3 = -1  # 3 flat
-            pos4 = -1  # 2 flat
-            for x in range(1, len(crest)):
-                if crest[x] == y_flat:
-                    count_flat += 1
-                    if up == 1:
-                        pos1 = x - 2
-                        up = 0
-                else:
-                    if crest[x] == y_flat + 1:
-                        up = 1
-                    elif crest[x] == y_flat + 2:
-                        pos2 = x - 1
-                    y_flat = crest[x]
-                    count_flat = 1
-                if count_flat == 3:
-                    pos3 == x - 2
-                if count_flat == 2:
-                    pos4 = x - 1
-
-            if pos1 != -1:
-                pass
-            elif pos2 != -1:
-                pass
-            elif pos3 != -1:
-                pass
-            elif pos4 != -1:
-                pass
-            else:
-                # random
-                pass
-
-        def rule_I(crest):
-            current_flat = crest[0]
-            current_Column = 0
-            flat = 1
-            for x in range(1, len(crest)):
-                if crest[x] == current_flat:
-                    flat += 1
-                else:
-                    current_flat = crest[x]
-                    current_Column = x
-                    flat = 1
-
-                if flat == 4:
-                    x_flat = current_Column
-
-                # if crest[x] <= current_L:
-                #     low_x = x
-                #     current_L = crest[x]
-            if flat >= 4:
-                # posiziona il pezzo in orrizontale a partire da current_Column
-                # return current_Column, current_flat,
-                pass
-            else:
-                current_L = max(crest)
-                low_x = 0
-                for x in range(1, len(crest)):
-                    if crest[x] <= current_L:
-                        low_x = x
-                        current_L = crest[x]
-                pass  # posiziona il pezzo in verticale nel punto più basso (current_L), quindi nel punto low_x
-
-        def rule_O(crest):
-            # posiziona il pezzo nel punto più basso possibile
-            current_flat = crest[0]
-            current_L = max(crest)
-            low_x = 0
-            current_Column = 0
-            flat = 1
-            for x in range(1, len(crest)):
-                if crest[x] == current_flat:
-                    flat += 1
-                else:
-                    current_flat = crest[x]
-                    current_Column = x
-                    flat = 1
-                if crest[x] <= current_L:
-                    low_x = x
-                    current_L = crest[x]
-                if flat == 2:
-                    x_flat = current_Column
-            if flat >= 2:
-                # posiziona il pezzo a partire da current_Column
-                pass
-            else:
-                # posiziona il pezzo nel punto più basso
-                pass
-
-        def rule_T(crest):  # 4 possivili rotazioni: punta a sx, dx, alto e in basso
-            # analizzo la crest
-            current_flat = crest[0]
-            current_L = max(crest)
-            low_x = 0
-            current_Column_flat = 0
-            count_flat = 1
-            x_up_stair = 0
-            x_down_stair = 0
-            x_down = 0
-            up_stair = crest[0] + 1
-            down_stair = crest[0] - 1
-
-            for x in range(1, len(crest)):
-                if crest[x] == current_flat:  # controllo se flat
-                    count_flat += 1
-                else:
-                    if crest[x] == up_stair and (not x_down_stair or (
-                            x_down_stair and x_down_stair != x - 1)):  # caso up_stair (controllo se non siamo finiti nel caso con la punta verso il basso)
-                        x_up_stair = x - 1
-                    elif crest[x] == up_stair and x_down_stair and x_down_stair == x - 1:  # caso down
-                        x_down = x
-                    elif crest[x] == down_stair:  # caso down_stair
-                        x_down_stair = x
-
-                    current_flat = crest[x]
-                    current_Column = x
-                    count_flat = 1
-                    if not x_up_stair:
-                        up_stair = crest[x] + 1
-                    if not x_down_stair:
-                        down_stair = crest[x] - 1
-
-                if x_down:
-                    # posiziona il pezzo con la punta nel punto x_down
+        def rule_S():
+            for shadow in shadows:
+                shape, rot, seq, priority = shadow
+                if shape == 'S':
                     pass
-                elif x_down_stair:
-                    # posiziona il pezzo in piedi con la punta più in basso in x_down_stair e ruotato a sx
-                    pass
-                elif x_up_stair:
-                    # posiziona il pezzo in piedi con la punta più in basso in x_up_stair e ruotato a dx
-                    pass
-                elif count_flat >= 3:
-                    # posiziona il pezzo sdraiato con il margine sinistro in current_column_flat
-                    pass
-                else:
-                    # posiziona il pezzo random
-                    pass
+        def rule_Z():
+            pass
+        def rule_J():
+            pass
+        def rule_L():
+            pass
+        def rule_I():
+            pass
+        def rule_O():
+            pass
+        def rule_T():
+            pass
 
         if piece['shape'] == 'S':
             return rule_S
@@ -296,7 +48,41 @@ class RuleBased(BaseGame):
         if piece['shape'] == 'T':
             return rule_T
 
-#restituisce una cresta con offset sullo start
+
+    def update_crest(self, heights):
+        self.crest = 0
+        for x in range(BOARDWIDTH):
+            self.crest[x] = heights[x] - heights[x-1]
+
+    def get_Pcrest(self): #MODIFICA
+        Pcrest = list()
+        #for x in range(int(len(self.crest))):  # 1
+            #Pcrest.append([self.crest[x]])
+        for x in range(int(len(self.crest) - 1)):  # 2
+            Pcrest.append((x, [0, self.crest[x + 1] - self.crest[x]]))
+        for x in range(int(len(self.crest) - 2)):  # 3
+            Pcrest.append((x, [0, self.crest[x + 1] - self.crest[x], self.crest[x + 2] - self.crest[x + 1]]))
+        for x in range(int(len(self.crest) - 3)):  # 4
+            Pcrest.append((x, [0, self.crest[x + 1] - self.crest[x], self.crest[x + 2] - self.crest[x + 1], self.crest[x + 3] - self.crest[x + 2]]))
+        return Pcrest
+
+    def get_heights(self):
+        heights = [0]*BOARDWIDTH
+        for x in range(BOARDWIDTH):
+            for y in range(BOARDHEIGHT):
+                if is_on_board(x, y):
+                    heights[x] = y
+        return heights
+
+
+
+
+
+
+
+
+
+
 
 
 
--- com/Utils/KnowledgeBase.py
+++ com/Utils/KnowledgeBase.py
@@ -1,37 +1,29 @@
 from collections import namedtuple
 
 #codifica le regole semplici
-# differenza tra il pezzo corrente e il pezzo precedente
-differences_ = {
-    -2: 'jump_down',
-    -1: 'down',
-    0: 'flat',
-    1: 'up',
-    2: 'jump_up',
-}
 
-differences = {
-    'jump_down': -2,
-    'down': -1,
-    'flat': 0,
-    'up': 1,
-    'jump_up': 2,
-}
+
+
+#shadows_S = {
+#    [0, 0, 1]: 1,
+#    [0, -1]: 2,
+#}
+
+
 
 #codifica le regole composte
 
 #crea dizionario con le regole
-
 #grandezza 4
-f_4 = ('flat', 'flat', 'flat', 'flat')
+f_4 = (0, 0, 0, 0)
 
 
 #grandezza 3
-f_3 = ('flat', 'flat', 'flat')
-f_d_u = ('flat', 'down', 'up')
-f_f_u = ('flat', 'flat', 'up')
-f_d_f = ('flat', 'down', 'flat')
-f_u_f = ('flat', 'up', 'flat')
+f_3 = (0, 0, 0)
+f_d_u = (0, -1, 0)
+f_f_u = (0, 0, 1)
+f_d_f = (0, -1, -1)
+f_u_f = (0, 1, 1)
 f_f_d = ('flat', 'flat', 'down')
 
 
@@ -91,26 +83,72 @@ shadows_T = {
 
 a = [0, 0, -1, 0, 1, 1, 0, 0, 0, 2]         #cresta
 
+
 Pa = list()                                 #insieme delle parti della cresta
 
+#non utilizziamo il riferimento assolumo ma quello relativo per definire la cresta
+#c[x] = b[x] - b[x-1]
+
 for x in range(int(len(a))):                #1
-    Pa.append([a[x]])
+    Pa.append((x, [a[x]]))
 
 for x in range(int(len(a) - 1)):            #2
-    Pa.append([a[x], a[x+1]])
+    Pa.append((x, [a[x], a[x+1]]))
 
 for x in range(int(len(a) - 2)):            #3
-    Pa.append([a[x], a[x+1], a[x+2]])
+    Pa.append((x, [a[x], a[x+1], a[x+2]]))
 
 for x in range(int(len(a) - 3)):            #4
-    Pa.append([a[x], a[x+1], a[x+2], a[x+3]])
+    Pa.append((x, [a[x], a[x+1], a[x+2], a[x+3]]))
 
-b = [1, 0, 0]                               #ombra del pezzo
+b = [1, 0, 0] #ombra del pezzo
 
-if b in Pa:
-    print('yes')                            #salvo la posizione
-else:
-    print('no')                             #non salvo la posizione
+c = [0]*10
+for x in range(10):
+    c[x] =[x, b]
 
-#analizzo l'altra ombra
+print('ombra b')
+print(b)
+
+print('Insieme delle parti')
+print(Pa)
+
+print('possibili posizioni di b')
+print(c)
+
+
+for pax in Pa:
+    start, seq = pax
+    print('start: ', str(start))
+    print('seq__: ', str(seq))
+
+
+Shadow = namedtuple('Shadow', [
+    'shape',
+    'rotation',
+    'sequence',
+    'priority'
+])
+
+shadows = (
+    Shadow('S', 0, [0, 0, 1], 1),
+    Shadow('S', 1, [0, -1], 0),
+    Shadow('Z', 0, [0, -1, 0], 1),
+    Shadow('Z', 1, [0, 1], 0),
+    Shadow('I', 0, [0, 0, 0, 0], 0),
+    Shadow('I', 1, [0], 0),
+    Shadow('O', 0, [0, 0], 0),
+    Shadow('J', 0, [0, 0, 0], 1),
+    Shadow('J', 1, [0, 2], 2),
+    Shadow('J', 2, [0, 0, -1], 3),
+    Shadow('J', 3, [0, 0], 0),
+    Shadow('L', 0, [0, 0, 0], 1),
+    Shadow('L', 1, [0, -2], 2),
+    Shadow('L', 2, [0, 1, 0], 3),
+    Shadow('L', 3, [0, 0], 0),
+    Shadow('T', 0, [0, 0, 0], 0),
+    Shadow('T', 1, [0, 1], 1),
+    Shadow('T', 2, [0, -1, 1], 3),
+    Shadow('T', 3, [0, -1], 2),
+)
 
