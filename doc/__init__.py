50a702e9722063b38c387a8a4489ddd5495985e3
chrism1c <christian-miccolis@libero.it>
2020-03-23 11:58:32 +0100

    Restyle font documentazione "TetrAIs"

---
+++ doc/__init__.py
@@ -0,0 +1,23 @@
+"""
+
+# Bibliografia
+"Artificial Intelligence: Foundations of Computational Agents"
+
+# Q-Learning
+"Deeplizard Reinforcement Learning - Goal Oriented Intelligence"
+"https://it.wikipedia.org/wiki/Q-learning"
+"https://towardsdatascience.com/reinforcement-learning-temporal-difference-sarsa-q-learning-expected-sarsa-on-python-9fecfda7467e"
+# SDG
+"https://it.wikipedia.org/wiki/Discesa_stocastica_del_gradiente"
+"https://towardsdatascience.com/stochastic-gradient-descent-clearly-explained-53d239905d31"
+"https://www.geeksforgeeks.org/ml-stochastic-gradient-descent-sgd/"
+
+# Ricerca Locale
+"https://www.okpedia.it/ricerca_locale_greedy"
+"https://medium.com/cracking-the-data-science-interview/an-introduction-to-optimization-in-intelligent-systems-c1aa408d3ac2"
+
+# Genetico
+"https://pdfs.semanticscholar.org/3e40/a48f6fa2c17e933dd6b1c707fd43aae9d176.pdf"
+
+
+"""
